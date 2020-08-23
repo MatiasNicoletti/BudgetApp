4d043c249a669bc00374a62bb1b4bcef12440817
Matias Nicoletti <matiasnicolettmdp@gmail.com>
2020-08-23 14:37:38 -0300

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './operation/list/list.component';
+import { AddComponent } from './operation/add/add.component';
+import { DeleteComponent } from './operation/delete/delete.component';
+import { BalanceComponent } from './balance/balance/balance.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    AddComponent, 
+    DeleteComponent,
+    BalanceComponent
   ],
   imports: [
     BrowserModule,
