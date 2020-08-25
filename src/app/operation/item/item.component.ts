17a9cb7fce7201d7875eba3a1146e5d0cb5a363c
Matias Nicoletti <matiasnicolettmdp@gmail.com>
2020-08-25 14:08:23 -0300

    Style of Modal popup to edit item complete

--- src/app/operation/item/item.component.ts
+++ src/app/operation/item/item.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Operation } from 'src/app/models/Operation';
 import { OperationService } from '../operation.service';
 import { BalanceService } from 'src/app/balance/balance.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-item',
@@ -10,15 +11,27 @@ import { BalanceService } from 'src/app/balance/balance.service';
 })
 export class ItemComponent implements OnInit {
   @Input() item:Operation;
+  editItemForm: FormGroup;
   constructor(private operationService: OperationService, private balanceService: BalanceService) { }
 
   ngOnInit(): void {
-  
+    this.initForm();
   }
 
   onDeleteItem(){
     this.operationService.deleteOperation(this.item);
     this.balanceService.removeItem(this.item);
   }
+  onEditItem(){
+    console.log(this.item);
+  }
 
+  initForm(){
+    this.editItemForm = new FormGroup(
+      {
+        description: new FormControl(this.item.description, [Validators.required, Validators.maxLength(15)]),
+        amount: new FormControl(this.item.amount, [Validators.required, Validators.pattern(/^[0-9]*$/)])
+      }
+    );
+  }
 }
